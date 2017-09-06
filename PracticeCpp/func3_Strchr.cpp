5ff85467ed14ced06e614d5f85fff4a059a89f2a
Renleilei_Johnny <renleilei1992@foxmail.com>
2017-09-06 23:48:22 +0800

    func3 update!

--- PracticeCpp/func3_Strchr.cpp
+++ PracticeCpp/func3_Strchr.cpp
@@ -11,26 +11,42 @@ using namespace std;
 int main(int argc,char **argv)
 {
 	char str[100] = "Today is Monday!";
-	char *p[10];
-	int i = 0;
-
+	char p[20] = {};
+	int i = 0, j = 0;
+	int count_space = 0;
+	int len = strlen(str) - 1;
+	int space_flag[] = {};
+	
 	cout<<"sizeof(str) = "<<sizeof(str)<<endl;
 	cout<<"sizeof(p) = "<<sizeof(p)<<endl;
+	cout<<"strlen(str) = "<<len<<endl;
 
+	memset(p, 0x00, sizeof(p)-1);
+	strcpy(p,str);
 	for(i=0; i<(sizeof(p)/sizeof(p[0])); ++i){
-		p[i] = (char *)malloc(sizeof(str));
-		memset(p[i], 0x00, sizeof(p[i]));
+//		p[i] = (char)malloc(sizeof(p[0]));
 //		strcpy(p[i],static_cast<char *>(&str[i]));
-		strcpy(p[i],&str[i]);
 //		strcpy(p[i],"h");
+		if(p[i] == '\0')
+			break;
+
 		cout<<" i= "<<i<<"  p["<<i<<"]="<<p[i]<<"  str[i]="<<str[i]<<endl;
+
+		if(p[i] == ' '){
+			count_space++;
+			space_flag[count_space-1]=i;
+		}
 	}
 
-
-	for(i=0; i<(sizeof(p)/sizeof(p[0]))+1; ++i){
-		cout<<"sizeof(p["<<i<<"] = "<<sizeof(p[i])<<endl;
-		free(p[i]);
-//		delete p[i];
+	cout<<"空格数量: "<<count_space<<endl;
+	for(i=count_space+1;i>0;i--){
+		if((i-count_space) >= 0){
+//			cout<<"空格位置: "<<space_flag[i-count_space]<<endl;
+			for(;j<(len-space_flag[i-count_space]);j++){
+				cout<<str[space_flag[i-count_space] + j];
+			}
+		}
+		cout<<" ";
 	}
 
 	return 0;
